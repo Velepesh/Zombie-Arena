6bad0cbc265283064ccaefc66e270560ebdff989
Velepesh <shepelev534@gmail.com>
2023-02-03 15:15:01 +0300

    ammend last commit
    This reverts commit 6b05e53ca99b359a62956a897c214c71c8bfcb4c.

--- Assets/Scripts/UI/HealthView/TextScaleAnimation.cs
+++ Assets/Scripts/UI/HealthView/TextScaleAnimation.cs
@@ -6,32 +6,44 @@ public class TextScaleAnimation : MonoBehaviour
 {
     [SerializeField] private float _startFontSize;
     [SerializeField] private float _targetFontSize;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _duration;
 
     private void OnValidate()
     {
         _startFontSize = Mathf.Clamp(_startFontSize, 0f, float.MaxValue);
         _targetFontSize = Mathf.Clamp(_targetFontSize, 0f, float.MaxValue);
-        _speed = Mathf.Clamp(_speed, 0f, float.MaxValue);
+        _duration = Mathf.Clamp(_duration, 0f, float.MaxValue);
     }
 
     public void PlayScaleAnimation(TMP_Text text)
     {
-        StartCoroutine(IncreaseFontSize(text));
+        StartCoroutine(EnableText(text));
     }
 
-    private IEnumerator IncreaseFontSize(TMP_Text text)
+    private IEnumerator EnableText(TMP_Text text)
     {
         text.fontSize = _startFontSize;
 
         while (text.fontSize < _targetFontSize)
         {
-            text.fontSize += Time.deltaTime * (_speed);
+            text.fontSize += Time.deltaTime * (_duration * 2);
 
             if (text.fontSize >= _targetFontSize)
             {
                 text.fontSize = _targetFontSize;
-                StartCoroutine(DecreaseFontSize(text));
+                break;
+            }
+
+            yield return null;
+        }
+
+        while (text.fontSize > _startFontSize)
+        {
+            text.fontSize -= Time.deltaTime * (_duration * 2);
+
+            if (text.fontSize <= _startFontSize)
+            {
+                text.fontSize = _targetFontSize;
                 break;
             }
 
@@ -39,19 +51,13 @@ public class TextScaleAnimation : MonoBehaviour
         }
     }
 
-    private IEnumerator DecreaseFontSize(TMP_Text text)
+    private void IncreaseFontSize()
     {
-        while (text.fontSize > _startFontSize)
-        {
-            text.fontSize -= Time.deltaTime * (_speed);
 
-            if (text.fontSize <= _startFontSize)
-            {
-                text.fontSize = _startFontSize;
-                break;
-            }
+    }
 
-            yield return null;
-        }
+    private void DecreaseFontSize()
+    {
+
     }
 }
